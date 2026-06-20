9970e0ce15d50e6ce726bec8ba491ae2aa4b0173
Jeanne Tucker <jltucke9@asu.edu>
2026-06-19 20:49:33 -0700

    checkin, scripts, styles
    working to develop the checkin pages, added script to be able to select the hidden checkin steps so i can further adjust the styles

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// show hidden checkin sections
+function showIntensity() {
+    document.getElementById("color-selection").classList.add("hidden");
+    document.getElementById("intensity-selection").classList.remove("hidden");
+}
+
+function showEnergy() {
+    document.getElementById("intensity-selection").classList.add("hidden");
+    document.getElementById("intensity-selection").classList.remove("hidden");
+}
+
+
+
+
+// event handlers
+document.getElementById("color-next-btn").addEventListener("click", showIntensity);
+
+document.getElementById("intensity-next-btn").addEventListener("click", showEnergy);
